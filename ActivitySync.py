08bfb7b88e18baf66b841af8794ab56c6e92a3b6
chazikai24 <kayyes@hotmail.com>
2025-04-29 23:42:00 +0800

    Update ActivitySync.py

--- ActivitySync.py
+++ ActivitySync.py
@@ -38,7 +38,7 @@ def syncData(username, password, garmin_email = None, garmin_password = None):
         garth.configure(domain="garmin.cn")
         garth.login(garmin_email, garmin_password)
         activities = garth.connectapi(
-            f"/activitylist-service/activities/search/activities?pageindex=2",
+            f"/activitylist-service/activities/search/activities",
             params={"activityType": "cycling", "limit": 10, "start": 0, 'excludeChildren': False},
         )
     else:
@@ -52,7 +52,7 @@ def syncData(username, password, garmin_email = None, garmin_password = None):
         res = session.post(url, data, headers=headers)
 
         # get igpsport list
-        url = "https://%s/Activity/ActivityList" % igp_host
+        url = "https://%s/Activity/ActivityList?pageindex=2" % igp_host
         res = session.get(url)
         result = json.loads(res.text, strict=False)
 
